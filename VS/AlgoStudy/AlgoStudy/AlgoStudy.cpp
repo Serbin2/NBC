ffde4dfb3c1c00de96f65aaccc1876d4c0680371
JIHONA\njh10 <njh1026@gmail.com>
2026-03-09 20:39:25 +0900

    blah blah

--- VS/AlgoStudy/AlgoStudy/AlgoStudy.cpp
+++ VS/AlgoStudy/AlgoStudy/AlgoStudy.cpp
@@ -5,98 +5,109 @@
 #include "Picnic.h"
 #include "BoardCover.h"
 
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <math.h>
 #include <vector>
 
-#include <math.h>
+using namespace std;
 
-int isPrime(int a)
+int node[101][101] = { 0, };
+int linked[101] = { 0, };	//	n에 연결된 노드의 수
+int desc[101] = { 0, };	//	나를 기준으로 전체 하위 노드의 수
+
+int CalcLinked(int n, int parent)
 {
-    //if(a==0)    return 0;
-    int s = sqrt((float)a)+1;
-    for (int i = 2; i < s; i++)
-    {
-        if ((a % i) == 0)
-        {
-            return 0;
-        }
-    }
-    return 1;
+	int ret = 1;
+	for (int i = 0; i < linked[n]; i++)
+	{
+		if (node[n][i] == parent)
+		{	//	부모 = 이미 방문한 노드
+			continue;
+		}
+
+		ret += (CalcLinked(node[n][i], n));
+	}
+	
+	desc[n] = ret;
+
+	return ret;
 }
 
-int PickNum(int depth, int nums[], int pickIndex, int picked[], size_t nums_len)
-{
-    if (depth == 3)
-    {
-        int result = picked[0] + picked[1] + picked[2];
-        return isPrime(result);
-    }
+// wires_rows는 2차원 배열 wires의 행 길이, wires_cols는 2차원 배열 wires의 열 길이입니다.
+int solution(int n, int** wires, size_t wires_rows, size_t wires_cols) {
+	int answer = -1;
+	
+	for (int i = 0; i < wires_rows; i++)
+	{
+		int a = wires[i][0];
+		int b = wires[i][1];
+		node[a][linked[a]] = b;	linked[a]++;	//	a노드에 연결된 노드의 수를 증가 & 해당 노드의 번호
+		node[b][linked[b]] = a;	linked[b]++;
+	}
 
-    int answer = 0;
-    int end = nums_len - (2 - depth);
-    for (int i = pickIndex; i < end; i++)
-    {
-        picked[depth] = nums[i];
-        answer += PickNum(depth + 1, nums, i + 1, picked, nums_len);
-    }
-    return answer;
+	CalcLinked(1, 0);
+	int min = 9999999;
+	for (int i = 0; i < 101; i++)
+	{
+		int sub = abs(n - (2 * desc[i]));
+		if (sub < min)
+		{
+			min = sub;
+		}
+	}
+	answer = min;
+	return answer;
 }
 
-// nums_len은 배열 nums의 길이입니다.
-int solution(int nums[], size_t nums_len) {
-    int pick[3] = { 0, };
-    int answer = PickNum(0, nums, 0, pick, nums_len);
-    return answer;
-}
 
 int main(void)
 {
-    int arr[4] = { 1,2,3,4 };
-    int ans = solution(arr, 4);
-
-    return 0;
+	
+	return 0;
 }
-
 
 /*
 int main()
 {
-    std::cout << "Hello World!\n";
+	std::cout << "Hello World!\n";
 
 
-    int numOfStudent = 6;
+	int numOfStudent = 6;
 
-    int friendsList[10][2] = { {0,1},{0,2},{1,2},{1,3},{1,4},{2,3},{2,4},{3,4},{3,5},{4,5} };
+	int friendsList[10][2] = { {0,1},{0,2},{1,2},{1,3},{1,4},{2,3},{2,4},{3,4},{3,5},{4,5} };
 
-    int out = picnic(numOfStudent, friendsList);
-    std::cout<< out;
-    std::cout<<std::endl;
+	int out = picnic(numOfStudent, friendsList);
+	std::cout<< out;
+	std::cout<<std::endl;
 
   
-    //  보드커버 테스트용 입력 만들기
-    std::vector<std::vector<int>> board;
+	//  보드커버 테스트용 입력 만들기
+	std::vector<std::vector<int>> board;
 
-    for (int i = 0; i < 10; i++)
-    {
-        std::vector<int> line;
-        for (int j = 0; j < 8; j++)
-        {
-            if (j == 0 || j == 7 || i == 0 || i == 9)
-            {
-                line.push_back(1);
-            }
-            else
-            {
-                line.push_back(0);
-            }
-        }
-        board.push_back(line);
-    }
+	for (int i = 0; i < 10; i++)
+	{
+		std::vector<int> line;
+		for (int j = 0; j < 8; j++)
+		{
+			if (j == 0 || j == 7 || i == 0 || i == 9)
+			{
+				line.push_back(1);
+			}
+			else
+			{
+				line.push_back(0);
+			}
+		}
+		board.push_back(line);
+	}
 
-    int bcOut = 0;
-    bcOut = BoardCover(board);
+	int bcOut = 0;
+	bcOut = BoardCover(board);
 
-    std::cout << bcOut;
-    std::cout << std::endl;
+	std::cout << bcOut;
+	std::cout << std::endl;
 
 
 }*/
